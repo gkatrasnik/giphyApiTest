b060e809f3a9ffdf44bb888bb3a45452a566b25f
Gasper Katrasnik <gkatrasnik@gmail.com>
2021-05-17 16:03:18 +0200

    js works, need to add css

---
+++ main.js
@@ -0,0 +1,27 @@
+const img = document.querySelector("img");
+const gifButton = document.getElementById("gifButton");
+const inputText = document.getElementById("input");
+
+gifButton.addEventListener("click", () => {
+  fetchGif(readInputText());
+});
+
+fetchGif();
+
+function readInputText() {
+  let searchTerm = inputText.value;
+  return searchTerm;
+}
+
+function fetchGif(search) {
+  fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=fcgdm73ShRzbtmRCmWOMD7NJt0EEmNJz&s="${search}"`,
+    { mode: "cors" }
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      img.src = response.data.images.original.url;
+    });
+}
